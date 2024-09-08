644e8e3eea52a9ba7176209d0f7073d6357e252b
Ankush073 <ankush.ankush2414@gmail.com>
2024-09-08 12:06:07 +0530

    add all changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 ALOOWED_HOST=['.vercel_app','now.sh','127.0.0.1','localhost']
 
 from dotenv import load_dotenv
