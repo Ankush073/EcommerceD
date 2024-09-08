008137bc9b670d277b1924acf4b4a69883ebecbf
Ankush073 <ankush.ankush2414@gmail.com>
2024-09-08 10:54:42 +0530

    add all changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 
 ALLOWED_HOSTS = []
+ALOOWED_HOST=['.vercel_app','now.sh','127.0.0.1','localhost']
 
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
@@ -72,10 +73,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES={
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydata',
+        'USER':'postgres',
+        'PASSWORD':'mydatabase',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -117,10 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 
 MEDIA_URL = '/images/'
 
