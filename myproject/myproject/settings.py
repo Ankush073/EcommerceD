9ac8f7456bfd61dbf8cf4538f2ba8ed4e932f794
Ankush073 <ankush.ankush2414@gmail.com>
2024-09-10 07:48:03 +0530

    commit all changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -66,26 +66,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES={
-      'default': {
+DATABASE_NAME = os.environ.get('DATABASE_NAME')
+DATABASE_USER= os.environ.get('DATABASE_USER')
+DATABASE_PASSWORD=os.environ.get('DATABASE_PASSWORD')
+DATABASE_HOST=os.environ.get('DATABASE_HOST')
+DATABASE_PORT=os.environ.get('DATABASE_PORT')
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydata',
-        'USER':'postgres',
-        'PASSWORD':'mydatabase',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'NAME': DATABASE_NAME,
+        'USER': DATABASE_USER,
+        'PASSWORD': DATABASE_PASSWORD,
+        'HOST':DATABASE_HOST,
+        'PORT': DATABASE_PORT,
     }
 }
 
