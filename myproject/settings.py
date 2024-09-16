fff85a4df590110be8f6951a1169def1f77ce43e
Ankush073 <ankush.ankush2414@gmail.com>
2024-09-16 08:32:05 +0530

    commit all changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +64,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASE_NAME = os.environ.get('DATABASE_NAME')
