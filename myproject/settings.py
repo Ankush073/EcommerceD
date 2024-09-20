09639083caccce520ffead9c1f37de591c862c4a
Ankush073 <ankush.ankush2414@gmail.com>
2024-09-20 08:16:01 +0530

    commit all changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 print(STATICFILES_DIRS)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
